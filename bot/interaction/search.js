608bfab22f7741cd6e67f127bacb21693ea53e6b
JonGiamp <jonathan.giamporcaro@gmail.com>
2017-05-10 21:21:33 +0200

    add platform regex

--- bot/interaction/search.js
+++ bot/interaction/search.js
@@ -1,11 +1,23 @@
-const platform = {
-  pc: /(.{3,12})#\d{4}(\s*)$/,
-  xbl: /./,
-  ps4: /./
-};
+// TODO:
+// renforcer la regex psn
+// add rechercher en keyword
 
-const instruction = /^(\s*)(chercher|search)(\s*)/;
+const getStats = async (pseudo, platform, region) => {
+  const res = await fetch(`https://ow-api.herokuapp.com/profile/${platform}/${region}/${pseudo}`);
+  const data = await res.json();
+  return data;
+}
+
+// model: keywords pc|xbl|psn
+const instruction = /^(\s*)(chercher|search|find)(\s*)((.{3,12})#\d{4}(\s*)|[a-zA-Z0-9 ]{1,15}|[a-zA-Z0-9_-]{3,16})(\s*)$/;
 
 module.exports = bot => {
-  console.log('A FAIRE');
+  bot.hear(instruction, (payload, chat) => {
+
+  });
 };
+
+
+
+// getStats('JonGiamp')
+  // .then( data => console.log(data))
