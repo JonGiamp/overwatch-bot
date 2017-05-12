7c2dc1ef2b48d1794db6dc20562413f27e0ecce0
JonGiamp <jonathan.giamporcaro@gmail.com>
2017-05-12 12:17:57 +0200

    'fix'

--- bot/key.js
+++ bot/key.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
   accessToken: 'EAADCcjfaZADsBAPeKeAvDUUoZCH3npFo4ZBFkgDt1J5ZBBX8JvTHnPDIy2JZCSwrDboPkI6qTfyyQt5aI0XIfTbhqAuLv81ZCCG33Vra5NFpnI3Dcpajl8gaCEiFLXtAoi7aV7Vb2FiMWO2PeLqlhqrjdnEqwsK9bNiXeJgRpvkwZDZD',
   verifyToken: 'TEST_BOT_TOKEN',
   appSecret: '46c8e6f38c923e241bc040d1a40b6e24',
